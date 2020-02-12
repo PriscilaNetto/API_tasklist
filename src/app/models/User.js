d72d11528c0fbcffd227b752fd7f61677cd0760c
Priscila Netto <priscila.nnetto@hotmail.com>
2020-02-12 13:39:37 -0300

    configuração do model/user

---
+++ src/app/models/User.js
@@ -0,0 +1,17 @@
+import Sequelize, { Model } from 'sequelize';
+
+class User extends Model {
+  static init(sequelize) { //recebe por parametro o sequelize
+    super.init(           //o super chama o método init da classe Model
+      {
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        password_hash: Sequelize.STRING,
+      },
+      {
+        sequelize,
+      }
+    );
+  }
+}
+export default User;
