f428728bb58c3d1aba409098be87fdaff6542452
Priscila Netto <priscila.nnetto@hotmail.com>
2020-02-13 10:14:34 -0300

    inclusão do model Task e a conexão com bd

--- src/database/index.js
+++ src/database/index.js
@@ -2,18 +2,22 @@ import Sequelize from 'sequelize';
 import databaseConfig from '../config/database';
 
 import User from '../app/models/User';
+import Task from '../app/models/Task';
 
-const models = [User]; // foi colocado em array porque poderá existir mais models.
+const models = [User, Task]; // foi colocado em array porque poderá existir mais models.
 
 class Database {
   constructor() {
     this.init();
   }
+
   init() {
     // conexão do banco de dados com os models
     this.connection = new Sequelize(databaseConfig);
 
-    models.map(model => model.init(this.connection));
+    models
+      .map(model => model.init(this.connection))
+      .map(model => model.associate && model.associate(this.connection.models));
   }
 }
 
